0512cfdefcd6b0fcd53b7c132d4f83d2b9ff519f
SOHTA0318 <nicosohta0318@gmail.com>
2023-07-19 18:06:57 -0700

    lesson34 sliding window

---
+++ sec5 Problem Solving patterns/sliding_window/maxSubArraySum.js
@@ -0,0 +1,10 @@
+// function maxSubArraySum(arr, num){
+// let max = 0
+// let track = 0
+// for(let i = 0; i < arr.length - num + 1; i++){
+// for (let j = 0; j < num; j++) {
+//   if(max < arr[i] + arr)
+  
+// }
+// }
+// }
