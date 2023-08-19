9238d607dd4a44ef27ee7269e509c5564a3affb8
SOHTA0318 <nicosohta0318@gmail.com>
2023-08-19 23:28:42 +0900

    lec 178 selection sort

--- Andrei course/sec13 Sort/exercise/bubbleSort.js
+++ Andrei course/sec13 Sort/exercise/bubbleSort.js
@@ -3,7 +3,14 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 // use for loop inside while loop until loop array.length
 function bubbleSort(array) {
 for (let i = 0; i < array.length; i++) {
-  
+  for (let j = 0; j < array.length; j++) {
+    if(array[j] > array[j +1]){
+      const a = array[j]
+array[j] = array[j + 1]
+array[j + 1] = a
+    }
+    
+  }
   
 }
 }
--- Andrei course/sec13 Sort/exercise/selectionSort.js
+++ Andrei course/sec13 Sort/exercise/selectionSort.js
@@ -1,16 +1,18 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function selectionSort(array) {
-  let min = array[0]
   for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      if(array[j] < min){
-        
+    let min = i
+    let temp = array[i]
+    for (let j = i + 1; j < array.length; j++) {
+      if(array[j] < array[min]){
+min = j
       }
-      
     }
-    
+    array[i] = array[min]
+    array[min] = temp
   }
 }
 
 selectionSort(numbers);
+console.log(numbers);
