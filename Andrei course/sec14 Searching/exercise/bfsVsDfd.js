8f3851f4b9bec121ae1a085d51048ae59451eeb4
SOHTA0318 <nicosohta0318@gmail.com>
2023-08-27 23:01:36 +0900

    BFS DFS

---
+++ Andrei course/sec14 Searching/exercise/bfsVsDfd.js
@@ -0,0 +1,12 @@
+//If you know a solution is not far from the root of the tree:
+BFS
+//If the tree is very deep and solutions are rare: 
+DFS
+//If the tree is very wide:
+DFS
+//If solutions are frequent but located deep in the tree:
+BFS
+//Determining whether a path exists between two nodes:
+DFS
+//Finding the shortest path:
+BFS
